7b56a801756d10f9aeeaf0466fe7e6f81932daa7
turfyman <turfyman@gmail.com>
2014-10-24 00:13:52 -0400

    First commit of changes to the turfyman-swayze branch
    Tons of changes, see README.md for more details.

--- README.md
+++ README.md
@@ -1,6 +1,22 @@
+turfyman-swayze
+======
+1.0.0:
+-A ton of modifications to the base theme, here's a few highlights
+  +Added the ability to use cover images
+  +Added disqus functionality to the theme
+  +Added an author page (not very happy with the look, but it's ok for now.)
+  +Added a nav bar with dropdown for social links (should work on mobile too)
+  +Rewrote a lot of the general.js file to add some features to the header slide animation
+  +Added related posts at the bottom of the single posts
+  +Modified and wrote a lot of really shitty CSS (please don't lambaste me! I'm not a web designer!)
+  
+I've tested the theme on FF, Chrome, iOS Safari, and IE10/11. The theme completely breaks on IE9 and I'm sure some older versions of Chrome/FF/Safari too. I'm not even going to attempt to fix this, as it would take me way more time than it's worth to me.
+
+As an aside, this is my first project and first commit on Github.  You'll have to pardon my extremely hacky (at best) attempt to get things to flow and work in CSS. I may have violated some rules along the way, so feel free to critique, fix and commit. I don't plan on spending a lot of time  supporting this project outside of my own use of the modified theme, but I'll try to fix issues as they're reported.
+
+
 swayze
 ======
-
 The first ever WooThemes theme for Ghost.
 
 * Designed by Cobus Bester
--- assets/js/general.js
+++ assets/js/general.js
@@ -5,24 +5,157 @@
 /*globals jQuery, document */
 (function ($) {
     "use strict";
+    var prevScrollTop = 0;
+    var scrollTimer = 0;
+    var lastScrollFireTime = 0;
+    var scrollUpCount = 0;
+	var st = $(document).scrollTop();
+	var scrollingUp = false;
+    var atBottom = false;
+    var isHidden = false;
+    var bottomThresh = 150;
+    //the down threshold is the number of px travel from the top before the floating div goes away
+    //the up threshold is the same except its the num of px from the top when the header comes back.
+    var imgMobileDownThresh = 100;
+    var imgMobileUpThresh = 400;
+    var imgDeskDownThresh = 300;
+    var imgDeskUpThresh = 400;
+    var noImgMobileDownThresh = 120;
+    var noImgMobileUpThresh = 280;
+    var noImgDeskDownThresh = 200;
+    var noImgDeskUpThresh = 800;
+    var isMobile = false;
+    
+    //check for supported browser for HW accel    
+    var thisBody = document.body || document.documentElement,
+            thisStyle = thisBody.style,
+            transitionEndEvent = 'webkitTransitionEnd transitionend',
+            cssTransitionsSupported = thisStyle.WebkitTransition !== undefined || thisStyle.transition !== undefined,
+            has3D = ('WebKitCSSMatrix' in window && 'm11' in new WebKitCSSMatrix());
+    //this is direct from the CSS, so if the CSS changes, this needs to change as well.        
+    function detectMobile(){
+            isMobile = window.matchMedia("only screen and (max-width: 767px)").matches || window.matchMedia("only screen and (max-device-width: 767px)").matches;
+    }   
+    
+   $(document).ready(function () {
+        detectMobile();
+        function isScrollingUp(){   
+			st = $(document).scrollTop();
+			if (st-prevScrollTop > 10){
+				//console.log("scrolling down");
+				scrollingUp = false;
+                scrollUpCount = 0;
+				prevScrollTop = st;
+			}
+			else if (prevScrollTop-st > 10)  {
+				//console.log("scrolling up");
+                //count twice before allowing to transition
+                if(scrollUpCount > 0){
+                    scrollingUp = true;
+                    scrollUpCount = 0;
+                }
+                 else{
+                    scrollUpCount++
+                }
+				prevScrollTop = st;
+			}
+		}
+		function checkPagePosition(downScrollThresh, upScrollThresh, isImg){
+			isScrollingUp();
+            if($(window).scrollTop() + $(window).height() >= $(document).height()){
+                atBottom = true;
+                //console.log("at bottom");
+            }
+            else if($(window).scrollTop() + $(window).height() < $(document).height() - bottomThresh){
+                atBottom = false;
+                //console.log("not at bottom");
+            }
+            if(isImg){
+                if (((st >= downScrollThresh && !scrollingUp) || atBottom) && !isHidden) {
+                   $('.image-header').removeClass('show');
+                   $('.image-header').addClass('hide');
+                   isHidden = true;
+                } // End If Statement
+                if (scrollingUp && st < upScrollThresh && $('.image-header').hasClass('hide') && !atBottom && isHidden){
+                   $('.image-header').removeClass('hide');
+                   $('.image-header').addClass('show');
+                   isHidden = false;
+                } // End If Statement
+            }
+            else{
+                if (((st >= downScrollThresh && !scrollingUp) || atBottom) && !isHidden) {
+                   $('.single-header').removeClass('show');
+                   $('.single-header').addClass('hide');
+                   isHidden = true;
+                } // End If Statement
+                if (scrollingUp && st < upScrollThresh && $('.single-header').hasClass('hide') && !atBottom && isHidden){
+                   $('.single-header').removeClass('hide');
+                   $('.single-header').addClass('show');
+                   isHidden = false;
+                } // End If Statement
+            }
+		}
+        if ($('.single-header').length) {
 
-	$(document).ready(function(){
+           $(document).on('touchmove touchend touchstart scroll', function () {
+             if(isMobile){
+                checkPagePosition(noImgMobileDownThresh,noImgMobileUpThresh,false);
+                //console.log("mobile scroll");
+             }
+             else{
+                checkPagePosition(noImgDeskDownThresh,noImgDeskUpThresh,false);
+                //console.log("desk scroll");
+             }
+            });
 
-		if ( $( '.single-header' ).length ) {
+        }  // End If Statement
+        
+        if ($('.image-header').length) {
+           $(document).on('touchmove touchstart touchend scroll', function () {
+                if(isMobile){
+                    checkPagePosition(imgMobileDownThresh,imgMobileUpThresh,true);
+                    //console.log("mobile scroll");
+                }
+                else{
+                    checkPagePosition(imgDeskDownThresh,imgDeskUpThresh,true);
+                   //console.log("desk scroll");
+                }
+            });
+        }
 
-			$( 'body' ).on( 'mousewheel', function() {
-		        if ( $( document ).scrollTop() >= 300 )  {
-		        	$( '.single-header' ).removeClass( 'show' );
-		        	$( '.single-header' ).addClass( 'hide' );
-		        } // End If Statement
-		        if ( ( $( document ).scrollTop() < 300 ) && ( $( document ).scrollTop() > 0 ) && ( $( '.single-header' ).hasClass( 'hide' ) ) )  {
-		        	$( '.single-header' ).removeClass( 'hide' );
-		        	$( '.single-header' ).addClass( 'show' );
-		        } // End If Statement
-		    });
-
-		} // End If Statement
-
-	});
+        // Switch to CSS3 Transform 3D if supported & accordion element exist
+        //This is for the mobile nav menu
+        if(cssTransitionsSupported && has3D ) {
+                if($("#menu-slider").length > 0) { 
+                        $("#menu-slider").addClass("accordion_css3_support");
+                }
+                if($("#socialbar-slider").length > 0) { 
+                        $("#socialbar-slider").addClass("accordion_css3_support");
+                }
+        }
+    
+        $( "#menu-bar" ).click(function() {
+			if ( $("#menu-toggle").css("display") != "none"){  //keeps the menu from toggling in non-mobile screen
+                //If browser not support transition or 3D transform (for webkit browser only)
+                if(!cssTransitionsSupported || !has3D ) {
+                        $("#menu-slider").slideToggle(650, 'easeOutBounce');
+                } else {
+                        $("#menu-slider").toggleClass("animated");
+                }
+			} //end if
+        });
+        $( "#social-menu-item" ).click(function() {
+			if ( $("#menu-toggle").css("display") != "block"){  //keeps the menu from toggling in mobile screen
+                //If browser not support transition or 3D transform (for webkit browser only)
+                if(!cssTransitionsSupported || !has3D ) {
+                        $("#socialbar-slider").slideToggle(650, 'easeOutBounce');
+                } else {
+                        $("#socialbar-slider").toggleClass("animated");
+                }
+			} //end if
+        });
+        //for the related posts "plugin"
+        $('.related-posts').ghostRelated();
+    });
 
 }(jQuery));
